65b23402faed10c462d0610a2a568b67603f87a4
David Wheatley <hi@davwheat.dev>
2021-03-07 18:46:15 +0000

    This isn't Flarumite! 🙈

--- extend.php
+++ extend.php
@@ -3,13 +3,13 @@
 /*
  * This file is part of davwheat/inline-group-badges.
  *
- * Copyright (c) 2021 Flarumite.
+ * Copyright (c) 2021 David Wheatley.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
-namespace Flarumite\InlineGroupBadges;
+namespace Davwheat\InlineGroupBadges;
 
 use Flarum\Extend;
 
